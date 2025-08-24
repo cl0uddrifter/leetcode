80664a43bccf5f777dc03e24091754a967a1859d
valleyx1 <cl0uddrifter@proton.me>
2025-08-24 14:34:50 -0700

    Create search-bst.py

---
+++ binary-search-tree/search-bst.py
@@ -0,0 +1,24 @@
+# Problem 700
+# Run   O(h)
+# Space O(h)
+# ... h = height of tree
+# Best: O(log n)
+# Worst: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if val == root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
