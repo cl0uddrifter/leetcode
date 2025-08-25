3427ee92e22892ce0ba4ede072c6780839caa720
valleyx1 <cl0uddrifter@proton.me>
2025-08-25 10:18:36 -0700

    Create gcd-strings.py

---
+++ array-strings/gcd-strings.py
@@ -0,0 +1,11 @@
+# Problem 1071
+# Run   O(n + m)
+# Space O(n + m)
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if (str1 + str2 != str2 + str1):
+            return ""
+        
+        n = gcd(len(str1), len(str2))
+        return str1[:n]
